2d4763a4d0bcb38d1fdc48d698f88cfa5a936a59
Tsu-Giriiii <153367525+Tsu-Giriiii@users.noreply.github.com>
2026-07-18 16:41:29 +0530

    Implement board rendering and basic move execution

---
+++ Giri_Chess/ChessEngine.py
@@ -0,0 +1,59 @@
+'''
+This file stores all the information about the current state of the Chess game.
+Also responsible to determining the valid moves in the current state. It will also keep a move log.
+'''
+
+class GameState:
+    def __init__(self):
+        #Numpy arrays based board will be faster for AI based engine
+        #Initial position of the board from white's perspective
+        self.board = [                                  
+            ["bR","bN","bB","bQ","bK","bB","bN","bR"],
+            ["bp","bp","bp","bp","bp","bp","bp","bp"],
+            ["--","--","--","--","--","--","--","--"],
+            ["--","--","--","--","--","--","--","--"],
+            ["--","--","--","--","--","--","--","--"],
+            ["--","--","--","--","--","--","--","--"],
+            ["wp","wp","wp","wp","wp","wp","wp","wp"],
+            ["wR","wN","wB","wQ","wK","wB","wN","wR"]
+        ]
+        #Note: Board is an 8x8, 2-D list
+        #Each cell is represented by two characters 1st: Color (b/w), 2nd: Piece type (K,Q,R,B,N,p)
+        #Empty square is represented by "--"
+        
+        self.whiteToMove = True
+        self.moveLog = []
+    
+    def make_move(self,move):
+        self.board[move.startrow][move.startcol] = '--' # make the space piece just left empty
+        self.board[move.endrow][move.endcol] = move.piece_moved
+        self.moveLog.append(move)               # maintain move history (to undo)
+        print(self.moveLog)
+        self.whiteToMove = not self.whiteToMove   #swap turns after a move
+        
+
+class Move:
+    
+    #dictonaries for notation conversion
+    rankstorows = {'1':7,'2':6,'3':5,'4':4,'5':3,'6':2,'7':1,'8':0}
+    rowstoranks = {7:'1',6:'2',5:'3',4:'4',3:'5',2:'6',1:'7',0:'8'}
+    
+    filestocols = {'a':0,'b':1,'c':2,'d':3,'e':4,'f':5,'g':6,'h':7}
+    colstofiles = {0:'a',1:'b',2:'c',3:'d',4:'e',5:'f',6:'g',7:'h'}
+    
+    
+    def __init__(self,startsq,endsq,board):
+        self.startrow = startsq[0]
+        self.startcol = startsq[1]
+        self.endrow = endsq [0]
+        self.endcol = endsq[1]
+        self.piece_moved = board[self.startrow][self.startcol]
+        self.piece_captured = board[self.endrow][self.endcol]      #this may even capture empty sqaures in which case nothing is captured
+        
+    
+    #Allows us to get the real chess notations
+    def Get_chessNotation(self):
+        return self.convertRankFile(self.startrow,self.startcol)+self.convertRankFile(self.endrow,self.endcol)
+    
+    def convertRankFile(self,r,c):
+        return self.colstofiles[c]+self.rowstoranks[r]
---
+++ Giri_Chess/ChessMain.py
@@ -0,0 +1,123 @@
+''' This is the driver file that handles input-output and GameState information'''
+
+import pygame as p  
+import ChessEngine
+
+p.init()
+HEIGHT = WIDTH = 512 #400 is also good
+DIMENSIONS = 8 #dimension of the chess board 8x8
+SQ_SIZE = HEIGHT // DIMENSIONS
+MAX_FPS = 15
+IMAGES = {}
+
+#Load the images
+#Initialize a global dictionary of images, it will be loaded only once to save computation
+
+def Load_Images ():
+    root = "Giri_Chess/Pieces/"
+    piece_list = ["bR","bN","bB","bQ","bK","bB","bN","bR","bp","wR","wN","wB","wQ","wK","wB","wN","wR","wp"]
+    for piece in piece_list:
+        IMAGES[piece] = p.transform.scale(p.image.load(root+piece+".png"),(SQ_SIZE,SQ_SIZE))
+
+#We can access the image of pieces using dictionary eg: IMAGES["wp"]
+#transform and scale the image to take up the whole square on the board
+
+#Main driver code: This will handle the user input and updating the graphics
+def main():
+    screen = p.display.set_mode((WIDTH,HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    p.display.update()
+    gs = ChessEngine.GameState()
+    print(gs.board)
+    Load_Images()   #Only do this once before the while loop
+    running = True
+    
+    #Store squares clicked
+    sq_selected = ()     #this will store the current square selected tuple (row,col)
+    player_clicks = []   # this will store starting pos, destination pos desired by the user[(3,6),(3,4)]
+    
+    #Game Loop
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            #mouse events
+            #click and drag  (add later)
+            if e.type ==p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()    #x,y location of the mouse
+                col = location[0]//SQ_SIZE
+                row = location[1]//SQ_SIZE
+                if sq_selected == (row,col):    # click the same square twice, unselect a sqaure
+                    sq_selected = ()
+                    player_clicks = []
+                else:                           # store the square clicked
+                    sq_selected = (row,col)
+                    player_clicks.append(sq_selected)
+                if len(player_clicks)==2:       # after 2nd click
+                    move = ChessEngine.Move(player_clicks[0],player_clicks[1],gs.board)
+                    print(move.Get_chessNotation())
+                    gs.make_move(move)
+                    sq_selected = ()    #reset user clicks
+                    player_clicks = []
+                    
+                
+        draw_Gamestate(screen,gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+        
+"""
+Responsible for graphics in used and displayed in the game
+"""
+def draw_Gamestate(screen,gs):
+    draw_Board(screen)              #To draw the squares on the board
+    #add in piece highlighting and move suggestions
+    draw_Pieces(screen,gs.board)    #draw the pieces on top of the Board
+ 
+
+"""
+Draw the squares on the board.
+"""  
+def draw_Board(screen):
+    #squares
+    '''for i in range(0,(DIMENSIONS+1)*SQ_SIZE,SQ_SIZE*2):
+        for j in range(0,(DIMENSIONS+1)*SQ_SIZE,SQ_SIZE*2):
+            p.draw.rect(screen,[255,255,200],[i,j,SQ_SIZE,SQ_SIZE])
+            p.draw.rect(screen,[50,150,50],[i,j-64,SQ_SIZE,SQ_SIZE])
+            p.draw.rect(screen,[50,150,50],[i-64,j,SQ_SIZE,SQ_SIZE])'''
+    
+    colors = [[255,255,200],[50,150,50]]
+    for r in range(DIMENSIONS):
+        for c in range(DIMENSIONS):
+            if (r+c)%2==0:
+                p.draw.rect(screen,colors[0],p.Rect(r*SQ_SIZE,c*SQ_SIZE,SQ_SIZE,SQ_SIZE))
+            else:
+                p.draw.rect(screen,colors[1],p.Rect(r*SQ_SIZE,c*SQ_SIZE,SQ_SIZE,SQ_SIZE))
+    
+    
+    
+    return screen
+
+"""
+Draw pieces on the board, using the current gamestate.board variable
+Also added ranks and files the remain constant on the board throughout the game
+"""
+def draw_Pieces(screen,board):          
+    for r in range(DIMENSIONS):
+        for c in range(DIMENSIONS):
+            piece = board[r][c]
+            if piece!='--': #Not empty square
+                screen.blit(IMAGES[piece],p.Rect(c*SQ_SIZE,r*SQ_SIZE,SQ_SIZE,SQ_SIZE))
+    
+    #ranks and files
+    font = p.font.SysFont(None,25)
+    s = "abcdefgh"
+    for r in range(DIMENSIONS):
+        rank = font.render(str(DIMENSIONS-r),True,[150,150,150])
+        screen.blit(rank,[0,r*SQ_SIZE])
+    for c in range(DIMENSIONS):
+        files = font.render(s[c],True,[150,150,150])
+        screen.blit(files,[c*SQ_SIZE+54,WIDTH-18])
+
+if __name__=="__main__":
+    main()
